c6afa10a9bf16c67994de6b4b52e5e325d3c5469
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-13 11:11:23 +0600

    Update 242_Valid_Anagram.py

--- 242_Valid_Anagram.py
+++ 242_Valid_Anagram.py
@@ -1,1 +1,10 @@
-print("Hi")
+def solve(str1, str2):
+    str3= sorted(str1)
+    str4= sorted(str2)
+
+    if str3 == str4:
+        return True
+    else:
+        return  False
+
+print(solve("rat", "car"))
