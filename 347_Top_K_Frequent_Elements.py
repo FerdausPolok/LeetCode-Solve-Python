6bdea18e3200ba9c780d3a7c0b339101aa1af9d2
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-19 19:52:26 +0600

    Update 347_Top_K_Frequent_Elements.py

--- 347_Top_K_Frequent_Elements.py
+++ 347_Top_K_Frequent_Elements.py
@@ -1,8 +1,27 @@
+def topKFrequent(nums, k):
+
+    dict={}
+    for i, v in enumerate(nums):
+        if v in dict:
+            dict[v]= dict.get(v)+1
+        else:
+            dict[v]=1
+    print(dict)
+    dict2= {k: v for k, v in sorted(dict.items(), key= lambda v: v[1], reverse= True)}
+    print(dict2)
+    flag =0
+    result = []
+    for i, v in dict2.items():
+        if flag == k:
+            break
+        else:
+            result.append(i)
+        flag +=1
+
+    return result
 
 
-dict = {
-    "1": 2,
-    "2": 3
-}
+print(topKFrequent([4,1,-1,2,-1,2,3], 2))
 
-print(dict(2))
+
+
